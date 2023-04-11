ebf6c775f921de8e37515b4c6d94054b79870c25
Statuxia <statuxia@ya.ru>
2023-04-11 19:58:54 +0300

    удален лишний метод

--- src/main/kotlin/algorytms/djkstra/Graph.kt
+++ src/main/kotlin/algorytms/djkstra/Graph.kt
@@ -4,11 +4,6 @@ class Graph {
     private val start = Node("start").setLength(0)
     private val end = Node("end")
 
-    fun addNode(nodeFrom: Node, nodeTo: Node, length: Int): Graph {
-        nodeFrom.setNode(nodeTo, length)
-        return this
-    }
-
     fun getStart(): Node {
         return start
     }
